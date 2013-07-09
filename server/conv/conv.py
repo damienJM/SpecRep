ac60d031e4000507422a40f60e746eab2137c474
Morten Albring <mortenalbring@gmail.com>
2013-07-09 12:03:28 +0100

    Changed Python error checking codes

---
+++ server/conv/conv.py
@@ -0,0 +1,117 @@
+#!/usr/bin/python
+
+import struct
+import sys
+import getopt
+import os
+
+def main(argv):
+    inputfile = ''
+    outputfile = ''
+
+#Get command line arguments
+    
+    try:
+        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+    except getopt.GetoptError:
+        print("Oh no! You didn't put in enough arguments or something!")
+        sys.exit(2)
+    for opt,arg in opts:
+        if opt == '-h':
+            print("conv.py -i inputfile -o outputfile");
+            sys.exit()
+        elif opt in ("-i","--ifile"):
+            inputfile = arg
+        elif opt in ("-o","--ofile"):
+            outputfile = arg
+#Test if input and output files have been specified properly
+            
+    if inputfile == '' :
+        print("Err1") #Input file not specified properly
+        sys.exit(2)
+    elif outputfile == '' :
+        print("Err2") #Output file not specified properly
+        sys.exit(2)
+
+    fileName, fileExtension = os.path.splitext(inputfile)
+
+    paramfile = fileName + ".DSC";
+
+
+#Test if input file exists
+    try:
+        with open(inputfile) : pass
+    except IOError:
+        print("Err3") #Input file doesn't exist
+        sys.exit(2)
+
+#Test if param file exists
+    try:
+        with open(paramfile) : pass
+    except IOError:
+        print("Err4") #Parameter file doesn't exist
+        sys.exit(2)
+
+    fin = open(paramfile,'r');
+    for line in fin :
+        
+        p = line[0:4]
+        #print(p)
+                
+        if p == 'XPTS' :
+            xpoints = int(line[5:len(line)]);
+  #          print(str(xpoints))
+            
+        if p == 'XMIN' :
+            xmin = float(line[5:len(line)]);
+    #        print(str(xmin))
+            
+        if p == 'XWID' :
+            xwid = float(line[5:len(line)]);
+   #         print(str(xwid))
+
+
+    xmax = xmin + xwid
+    xsampling = xwid / xpoints
+
+    xdata = [];
+    for k in range(1,xpoints,1) :
+        xdata.append(xmin + (xsampling * (k - 1)))
+  #      print(xdata[k-1])
+        
+#Test if file is DTA        
+    fileName, fileExtension = os.path.splitext(inputfile)
+  #  print(fileExtension)
+    if fileExtension == ".DTA" :
+      #  print("Now converting DTA file!");
+
+        toggle = 2;
+        ydata = [];
+        fin = open(inputfile,'rb');
+        
+        with open(inputfile,'rb') as inh:
+            indata = inh.read()
+        for i in range(0,len(indata),8) :
+            pos = struct.unpack('>d',indata[i:i+8])           
+        
+            ydata.append(pos[0]);
+        fin.close()
+    else :
+        print("Err5") #Not a DTA file
+        sys.exit(2)
+        
+#Write stuff to files
+    
+    fout = open(outputfile,'w');
+ #   print(str(len(xdata)))
+ #   print(str(len(ydata)))
+
+    for i in range(0,len(xdata)):
+        fout.write(str(xdata[i]))
+        fout.write(",")
+        fout.write(str(ydata[i]))
+        fout.write("\n");
+            
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
---
+++ server/conv/runpython.php
@@ -0,0 +1,41 @@
+<?php
+
+
+function checkfile($result) {
+
+	echo substr($result,0,-1);
+
+	if (substr($result,0,-1) == 'Err1') {
+		throw new Exception('Error 1 - Input file not specified properly in command line');
+	}
+	if (substr($result,0,-1) == 'Err2') {
+		throw new Exception('Error 2 - Output file not specified properly in command line');
+	}
+	if (substr($result,0,-1) == 'Err3') {
+		throw new Exception('Error 3 - There is no such input file');
+	}
+	if (substr($result,0,-1) == 'Err4') {
+		throw new Exception('Error 4 - There is no parameter file');
+	}
+	if (substr($result,0,-1) == 'Err5') {
+		throw new Exception('Error 5 - That is not a DTA file');
+	}
+	
+	return 'Success';
+}
+
+try {
+	$w = checkfile(shell_exec('C:\Python33\python.exe conv.py -i g.DTA -o output.txt'));
+	echo $w;
+	//$resultData = json_decode($result, true);
+	//var_dump($resultData);
+
+	
+} catch (Exception $e) {
+	echo 'Caught exception: ',  $e->getMessage(), "\n";
+}
+
+
+
+
+?>
